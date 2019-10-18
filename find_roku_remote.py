bcf000f4ecab434f0e5db9b942a2db0950ee47c8
jed <jed@m6700.lan>
2019-10-18 03:01:00 -0400

    .

--- find_roku_remote.py
+++ find_roku_remote.py
@@ -1,15 +1,7 @@
 #!virtual_env/bin/python
-
 import roku
 import time
-
 print("🚦", end="")
-
-cfg = {
-    "host": "192.168.1.128",
-    "port": 8060,
-    "timeout": 10, # E   
-}
 # Autofind roku.
 r = next(r for r in roku.Roku.discover() if r.port == 8060)
 print(".", end="")
